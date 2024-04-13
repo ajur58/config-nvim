b6d8a095d974c10f4b50783137064cc4590ddb94
Adam Juras <adam@hipwerk.com>
2024-04-13 11:30:41 +0200

    Small changes

--- lua/ajur58/packer.lua
+++ lua/ajur58/packer.lua
@@ -39,7 +39,7 @@ require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         requires = { { 'nvim-lua/plenary.nvim' }, 'BurntSushi/ripgrep' }
     }
-
+    -- for light theme try https://github.com/rmehri01/onenord.nvim
     use({
         'rose-pine/neovim',
         as = 'rose-pine',
