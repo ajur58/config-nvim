1f6a0a58f4df488a57aa29fdcbc90d5712726c72
Adam Juras <adam@hipwerk.com>
2024-10-19 18:23:31 +0200

    Removed tsserver

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,93 +1,100 @@
 require('mason').setup()
 
 require('mason-lspconfig').setup({
-    ensure_installed = {
-        -- Replace these with whatever servers you want to install
-        'elixirls',
-        'rust_analyzer',
-        'tsserver',
-        --'tailwindcss'
-    }
+  ensure_installed = {
+    -- Replace these with whatever servers you want to install
+    'elixirls',
+    'rust_analyzer',
+    'marksman'
+    --'tailwindcss'
+  }
 })
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp.setup {
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            --           require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp_mappings,
-    sources = {
-        { name = 'path' },
-        { name = 'nvim_lsp', keyword_length = 4 },
-        { name = 'buffer',   keyword_length = 3 },
-        { name = 'vsnip' }, -- For vsnip users.
-        --{ name = 'luasnip', keyword_length = 2 },
-    }
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)       -- For `vsnip` users.
+      --           require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp_mappings,
+  sources = {
+    { name = 'path' },
+    { name = 'nvim_lsp', keyword_length = 4 },
+    { name = 'buffer',   keyword_length = 3 },
+    { name = 'vsnip' },     -- For vsnip users.
+    --{ name = 'luasnip', keyword_length = 2 },
+  }
 }
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsp_attach = function(client, bufnr)
-    -- Create your keybindings here...
-    local opts = { buffer = bufnr, remap = false }
+  -- Create your keybindings here...
+  local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end
 
 local lspconfig = require('lspconfig')
 require('mason-lspconfig').setup_handlers({
-    function(server_name)
-        lspconfig[server_name].setup({
-            on_attach = lsp_attach,
-            capabilities = lsp_capabilities,
-        })
-    end,
+  function(server_name)
+    lspconfig[server_name].setup({
+      on_attach = lsp_attach,
+      capabilities = lsp_capabilities,
+    })
+  end,
 })
 
 -- Fix Undefined global 'vim'
 lspconfig.lua_ls.setup({
-    on_attach = lsp_attach,
-    capabilities = lsp_capabilities,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+  on_attach = lsp_attach,
+  capabilities = lsp_capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 })
 
 lspconfig.html.setup({
-    on_attach = lsp_attach,
-    capabilities = lsp_capabilities,
-    filetypes = { "html", "jsx" }
+  on_attach = lsp_attach,
+  capabilities = lsp_capabilities,
+  filetypes = { "html", "jsx" }
 })
+
+lspconfig.marksman.setup({
+  on_attach = lsp_attach,
+  capabilities = lsp_capabilities,
+  filetypes = { "markdown", "markdown.mdx" }
+})
+
 --[[
 lspconfig.tailwindcss.setup {
     on_attach = lsp_attach,
@@ -95,5 +102,5 @@ lspconfig.tailwindcss.setup {
 }--]]
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
